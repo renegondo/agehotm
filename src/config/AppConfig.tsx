cfbed254018e89238704f5021eea707077a9101e
renegondo <75799741+renegondo@users.noreply.github.com>
2025-02-11 10:51:14 -0600

    Update AppConfig.tsx
    SE CAMBIO EL WEBHOOK

--- src/config/AppConfig.tsx
+++ src/config/AppConfig.tsx
@@ -7,7 +7,7 @@ import React, { createContext, useContext, useState } from 'react';
     }>({
       config: {
         registrationWebhook: 'https://n8n.prospectos.pro/webhook/844c226b-f793-4759-abfc-a83ac1f2a884',
-        agentConfigWebhook: 'https://n8n.prospectos.pro/webhook/117383b5-998e-4fa3-a959-24e89dcb545b',
+        agentConfigWebhook: 'https://n8n.prospectos.pro/webhook/7dadf219-7fac-4e17-bc33-dea4670329e0',
         agentUpdateWebhook: 'https://n8n.prospectos.pro/webhook/agent-update',
         qrWebhook: 'https://n8n.prospectos.pro/webhook/f356ac57-4f9b-463e-8c6d-13eeeebbfe60',
         openaiApiKey: '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',
